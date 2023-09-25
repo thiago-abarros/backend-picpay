9c8e41fd62294deda19f737eca8482017aa76a88
Thiago Barros <bunbismuth@proton.me>
2023-09-25 16:43:03 -0300

    feat: create UserRepository

---
+++ src/main/java/com/projeto/spring/backendpicpay/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.spring.backendpicpay.repositories;
+
+import com.projeto.spring.backendpicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
