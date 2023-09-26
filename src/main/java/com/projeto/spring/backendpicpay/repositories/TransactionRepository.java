4cf9d64e34f561559cf52793e7677d4bfc592824
Thiago Barros <bunbismuth@proton.me>
2023-09-25 21:43:11 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/projeto/spring/backendpicpay/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.spring.backendpicpay.repositories;
+
+import com.projeto.spring.backendpicpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {   
+}
