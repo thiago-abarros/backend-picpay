df5afb015be0c2a98e0d48159082a8281bcb259b
Thiago Barros <bunbismuth@proton.me>
2023-10-27 15:05:37 -0300

    fix: removed the authorizeTransaction method
    Removed the authorizeTransaction method for porpouses of decoupling

--- src/main/java/com/projeto/spring/backendpicpay/services/TransactionService.java
+++ src/main/java/com/projeto/spring/backendpicpay/services/TransactionService.java
@@ -5,14 +5,9 @@ import com.projeto.spring.backendpicpay.domain.user.User;
 import com.projeto.spring.backendpicpay.dtos.TransactionDTO;
 import com.projeto.spring.backendpicpay.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -23,7 +18,7 @@ public class TransactionService {
     private TransactionRepository repository;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authService;
 
     @Autowired
     private NotificationService notificationService;
@@ -33,7 +28,7 @@ public class TransactionService {
         User receiver = this.userService.findUserById(transaction.receiverId());
 
         userService.validateTransaction(sender, transaction.value());
-        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        boolean isAuthorized = this.authService.authorizeTransaction(sender, transaction.value());
         if(!isAuthorized){
             throw new Exception("Transação não autorizada.");
         }
@@ -55,14 +50,5 @@ public class TransactionService {
         this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
 
         return newtransaction;
-    }
-
-    public boolean authorizeTransaction(User sender, BigDecimal value){
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
-
-        if (authorizationResponse.getStatusCode() == HttpStatus.OK){
-            String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado".equalsIgnoreCase(message);
-        } else return false;
     }
 }
